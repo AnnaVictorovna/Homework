e71afd211016567cd42ad200281f330258d9cfb3
AnnaVictorovna <125558041+AnnaVictorovna@users.noreply.github.com>
2023-03-01 11:26:36 +0300

    Add files via upload

---
+++ vvod_vyvod/Homework_1.java
@@ -0,0 +1,12 @@
+package by.vvod.vvod_vyvod;
+
+import java.util.Scanner;
+
+public class Homework_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println(in.nextLine());
+        }
+    }
+}
