1cf8eb702e9d9378b0c209fd6ae613685a45f83f
AnnaVictorovna <125558041+AnnaVictorovna@users.noreply.github.com>
2023-03-06 14:31:35 +0300

    Add files via upload

---
+++ homework/Homework_2__.java
@@ -0,0 +1,25 @@
+package by.vvod.vvod_vyvod;
+
+import java.util.Scanner;
+
+public class Homework_2__ {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число или \"stop\" для подсчёта среднего арифметического введенных чисел");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int summa = 0;
+            int count =0;
+            while (!scanner.hasNext("stop")) {
+                try {
+                        int number = scanner.nextInt();
+                    summa+=number;
+                    count++;
+                } catch (Exception e) {
+                    System.err.println("Это не целое число, попробуйте ещё раз!");
+                    scanner.next();
+                }
+            }
+            System.out.println((double) (summa/count));
+        }
+    }
+}
